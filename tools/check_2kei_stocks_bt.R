b6233036502d24105aaa14e20ace75b306481b20
fshin3 <fshin3@gmail.com>
2022-02-07 16:20:26 +0900

    add 3 results of sequential ABCs

--- tools/check_2kei_stocks_bt.R
+++ tools/check_2kei_stocks_bt.R
@@ -15,13 +15,13 @@ for(i in 1:length(Stocks)){
   cpuetmp <- cpuetmp[-na.omit(cpuetmp)]
   if(length(cpuetmp) <=4 ) next
   ccdata<-data.frame(year=ccdata.stock[[i]]$Year,cpue=ccdata.stock[[i]]$CPUE,catch=ccdata.stock[[i]]$Catch)
-  filename<-paste0("~/Desktop/2kei-stocks/",Stocks[i],"_default_bt5year.png")
-  resabc2 <-calc_abc2(ccdata,BTyear=(max(ccdata$year)-4),tune.par = c(0.5,0.4,0.4),summary_abc = F)
+  filename<-paste0("~/Desktop/2kei-stocks/",Stocks[i],"_2ndbest_bt5year.png")
+  resabc2 <-calc_abc2(ccdata,BTyear=(max(ccdata$year)-4),tune.par = c(0.4,0.4,0.8),summary_abc = F)
   #resabc2 <-calc_abc2(ccdata,summary_abc = F)
   graph_abc2 <-plot_abc2_fixHC_seqOut(resabc2)
   ABCs[[i]]<-graph_abc2[[1]]
   ABCs[[i]]$stock <- rep(Stocks[i],nrow(ABCs[[i]]))
-  ABCs[[i]]$tunepar <- rep(str_c("0.5-0.4-0.4"),nrow(ABCs[[i]]))
+  ABCs[[i]]$tunepar <- rep(str_c("0.4-0.4-0.8"),nrow(ABCs[[i]]))
   ABCs[[i]]$ABCdeviation <- (ABCs[[i]]$ABC-ABCs[[i]]$ABC[1])/ABCs[[i]]$ABC[1]
   ori.catch <- ccdata$catch
   l.catch <- length(ori.catch)
@@ -34,7 +34,10 @@ for(i in 1:length(Stocks)){
 
 #ABCs1st<-ABCs
 #ABCs2nd<-ABCs
-ABCsdefault<-ABCs
+#ABCsdefault<-ABCs
+save(ABCs1st,file = "./tools/seqOutABCs_bt5best.rda")
+save(ABCs2st,file = "./tools/seqOutABCs_bt52ndbest.rda")
+save(ABCsdefault,file = "./tools/seqOutABCs_default.rda")
 
 # for(i in 1:length(Stocks)){
 #   if(is.null(ABCs[[i]])) next
