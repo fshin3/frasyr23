2ac90d6f47a9b7c90ab8171044c79ad3ba436fc4
fshin3 <fshin3@gmail.com>
2022-02-09 17:47:17 +0900

    bit modify check_2kei_stocks_bt.r

--- tools/check_2kei_stocks_bt.R
+++ tools/check_2kei_stocks_bt.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-datafile <- read.csv("~/SkyDrive/2-kei-issues/type2_data.csv")
+datafile <- read.csv("~/OneDrive/2-kei-issues/type2_data.csv")
 
 Stocks <-unique(datafile$Stock)
 
@@ -7,6 +7,15 @@ ccdata.stock<-list()
 for(i in 1:length(unique(datafile$Stock))){
   ccdata.stock[[i]] <- datafile %>% filter(Stock==Stocks[i])
 }
+
+tune.pars <- list()
+tune.pars[[1]] <-c(0.5,0.4,0.4)
+tune.pars[[2]] <-c(0.1,0.5,0.9)
+tune.pars[[3]] <-c(0.4,0.4,0.8)
+parameans <- c("default","best","2ndbest")
+
+for(j in 1:length(tune.pars)){
+
 ABCs<-list()
 n.catch<-5
 for(i in 1:length(Stocks)){
@@ -15,13 +24,13 @@ for(i in 1:length(Stocks)){
   cpuetmp <- cpuetmp[-na.omit(cpuetmp)]
   if(length(cpuetmp) <=4 ) next
   ccdata<-data.frame(year=ccdata.stock[[i]]$Year,cpue=ccdata.stock[[i]]$CPUE,catch=ccdata.stock[[i]]$Catch)
-  filename<-paste0("~/Desktop/2kei-stocks/",Stocks[i],"_2ndbest_bt5year.png")
-  resabc2 <-calc_abc2(ccdata,BTyear=(max(ccdata$year)-4),tune.par = c(0.4,0.4,0.8),summary_abc = F)
+  filename<-paste0("~/Desktop/2kei-stocks/",Stocks[i],"_",parameans[j],"_bt5year.png")
+  resabc2 <-calc_abc2(ccdata,BTyear=(max(ccdata$year)-4),tune.par = tune.pars[[j]],summary_abc = F)
   #resabc2 <-calc_abc2(ccdata,summary_abc = F)
   graph_abc2 <-plot_abc2_fixHC_seqOut(resabc2)
   ABCs[[i]]<-graph_abc2[[1]]
   ABCs[[i]]$stock <- rep(Stocks[i],nrow(ABCs[[i]]))
-  ABCs[[i]]$tunepar <- rep(str_c("0.4-0.4-0.8"),nrow(ABCs[[i]]))
+  ABCs[[i]]$tunepar <- rep(str_c(paste0(tune.pars[[j]][1],"-",tune.pars[[j]][2],"-",tune.pars[[j]][3])),nrow(ABCs[[i]]))
   ABCs[[i]]$ABCdeviation <- (ABCs[[i]]$ABC-ABCs[[i]]$ABC[1])/ABCs[[i]]$ABC[1]
   ori.catch <- ccdata$catch
   l.catch <- length(ori.catch)
@@ -32,9 +41,11 @@ for(i in 1:length(Stocks)){
   #ggsave(width=420,height=150,dpi=200,units="mm", graph_abc2[[3]],file=filename)
 }
 
-#ABCs1st<-ABCs
-#ABCs2nd<-ABCs
-#ABCsdefault<-ABCs
+if(j==1) ABCsdefault<-ABCs
+else if(j==2) ABCs1st<-ABCs
+else ABCs2nd<-ABCs
+}
+
 save(ABCs1st,file = "./tools/seqOutABCs_bt5best.rda")
 save(ABCs2nd,file = "./tools/seqOutABCs_bt52ndbest.rda")
 save(ABCsdefault,file = "./tools/seqOutABCs_default.rda")
@@ -43,6 +54,21 @@ load("./tools/seqOutABCs_bt5best.rda")
 load("./tools/seqOutABCs_bt52ndbest.rda")
 load("./tools/seqOutABCs_default.rda")
 
+# check diffs in indices between default and bt5yr opt
+ABC1dif <- ABC2dif <- Catch1dif <- Catch2dif <-0
+for(i in 1:length(Stocks)){
+  if(is.null(ABCs1st[[i]])) next
+  for(j in 1:nrow(ABCs1st[[i]])){
+     if(ABCs1st[[i]]$ABCdeviation[j]-ABCsdefault[[i]]$ABCdeviation[j] < -0.01) {#print(paste("1st",Stocks[i],ABCs1st[[i]]$label[j]))
+    ABC1dif <-ABC1dif+1}
+     if((ABCs2nd[[i]]$ABCdeviation[j]-ABCsdefault[[i]]$ABCdeviation[j]) < -0.01) {#print(paste("ABC 2nd",Stocks[i]),ABCs2nd[[i]]$label[j])
+    ABC2dif<-ABC2dif+1}
+    if((ABCs1st[[i]]$Catch5yrdeviation[j]-ABCsdefault[[i]]$Catch5yrdeviation[j]) < -0.01) {#print(paste("1st",Stocks[i],ABCs1st[[i]]$label[j]))
+      Catch1dif <-Catch1dif+1}
+    if((ABCs2nd[[i]]$Catch5yrdeviation[j]-ABCsdefault[[i]]$Catch5yrdeviation[j]) < -0.01){ #print(paste("Catch 2nd",Stocks[i]),ABCs2nd[[i]]$label[j])
+      Catch2dif <-Catch2dif+1}
+  }
+}
 
 for(i in 1:length(Stocks)){
   if(is.null(ABCs1st[[i]])) next
@@ -55,9 +81,9 @@ for(i in 1:length(Stocks)){
   bt52ndCatchdev<-ABCs2nd[[i]]$Catch5yrdeviation
   ABCDevs<-data.frame(label=labels,baseABCdev=defaultABCdev,fix1ABCdev=bt51stABCdev,fix2ABCdev=bt52ndABCdev,baseCatchdev=defaultCatchdev,fix1Catchdev=bt51stCatchdev,fix2Catchdev=bt52ndCatchdev)
   filename<-paste0("~/Desktop/2kei-stocks/",Stocks[i],".csv")
-  write.csv(ABCDevs,file = filename)
+  #write.csv(ABCDevs,file = filename)
 
   gg.ABCdev <- ggplot(data=data.frame(X=c(-1,1))) +
-                geom_point(x=ABCDevs$label,y=ABCDevs$baseABCdev)
+                geom_point(y=ABCDevs$baseABCdev)
 
 }
